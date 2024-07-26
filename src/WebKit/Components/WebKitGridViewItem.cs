5bc13e06db0c386fc4073bde23696c4c0f5198f0
Nico <10356831+ferociousbyte@users.noreply.github.com>
2024-07-26 16:45:47 +0200

    Add WebKitGridView and WebKitGridViewItem components
    Introduce `WebKitGridView.razor` for displaying grid view items and `WebKitGridViewItem.cs` to define the grid view item model. Updated project file to include these new components.

---
+++ src/WebKit/Components/WebKitGridViewItem.cs
@@ -0,0 +1,12 @@
+﻿using Talaryon.WebKit.Models;
+
+namespace Talaryon.WebKit.Components;
+
+public class WebKitGridViewItem
+{
+    public required string? Title { get; set; }
+    public required string? Subtitle { get; set; }
+    public required DirectusFile Image { get; set; } = new();
+    public required string? Text { get; set; }
+    public string? Url { get; set; }
+}
