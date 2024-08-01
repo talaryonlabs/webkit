24d0bd2dc7244a2cf93044731d7212656cd6a360
Nico <10356831+ferociousbyte@users.noreply.github.com>
2024-08-02 01:42:31 +0200

    Add WebKitGallery component
    Introduced WebKitGallery.razor and IWebKitGalleryItem interface to support a new gallery feature. This includes dynamic image loading and a modal viewer for image display. Also updated the .csproj file to include these new components.

---
+++ src/WebKit/Components/IWebKitGalleryItem.cs
@@ -0,0 +1,9 @@
+﻿using Talaryon.WebKit.Models;
+
+namespace Talaryon.WebKit.Components;
+
+public interface IWebKitGalleryItem
+{
+    public DirectusFile Image { get; set; }
+    public int Sort { get; set; }
+}
