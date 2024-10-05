8e71ffaf9f35af7b0c28c52241627b921f254f42
Nico <10356831+ferociousbyte@users.noreply.github.com>
2024-10-05 08:16:55 +0200

    Refactor WebKitPriceViewLabels to include default values
    Updated WebKitPriceViewLabels to initialize properties with default values directly in the class definition, simplifying code in WebKitPriceView. Additionally, changed the property name from VATLabel to TaxLabel for clarity.

--- src/WebKit/Components/WebKitPriceViewLabels.cs
+++ src/WebKit/Components/WebKitPriceViewLabels.cs
@@ -2,8 +2,8 @@
 
 public class WebKitPriceViewLabels
 {
-    public string PricingLabel { get; set; }
-    public string IncludingLabel { get; set; }
-    public string ExcludingLabel { get; set; }
-    public string VATLabel { get; set; }
+    public string PricingLabel { get; set; } = "Paketpreis";
+    public string IncludingLabel { get; set; } = "im Paketpreis enthalten";
+    public string ExcludingLabel { get; set; } = "nicht im Paketpreis enthalten";
+    public string TaxLabel { get; set; } = "exkl. MwSt.";
 }
