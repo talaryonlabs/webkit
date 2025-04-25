700e8b8acd40588fab3642788a2ae77fae4c00a0
Nico <10356831+ferociousbyte@users.noreply.github.com>
2025-04-25 23:39:30 +0200

    Refactor logging and improve data models and gallery sorting.
    Moved logging outside of try blocks in Directus service methods for better clarity. Added Width and Height properties to the DirectusFile model with default values and JSON serialization settings. Introduced a new parameter, IsSortedList, in WebKitGallery to toggle item sorting dynamically.

--- src/WebKit/Models/DirectusFile.cs
+++ src/WebKit/Models/DirectusFile.cs
@@ -1,7 +1,18 @@
-﻿namespace Talaryon.WebKit.Models;
+﻿using System.ComponentModel;
+using System.Text.Json.Serialization;
+
+namespace Talaryon.WebKit.Models;
 
 public class DirectusFile
 {
     public string? Id { get; set; }
     public string? Title { get; set; }
+    
+    [DefaultValue(0)]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? Width { get; set; }
+    
+    [DefaultValue(0)]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? Height { get; set; }
 }
