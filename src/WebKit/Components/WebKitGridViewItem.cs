b92c2ca94ef74181d4b4a39f2c4d869314a8a71b
ferociousbyte <ferociousbyte@talaryonlabs.com>
2025-05-25 09:49:07 +0200

    Remove `required` modifier from `Subtitle` property
    The `Subtitle` property in `WebKitGridViewItem` is now optional, making it nullable by removing the `required` modifier. Updated `WebKitGridView` to conditionally render the `Subtitle` badge only when the property is not null.

--- src/WebKit/Components/WebKitGridViewItem.cs
+++ src/WebKit/Components/WebKitGridViewItem.cs
@@ -5,8 +5,8 @@ namespace Talaryon.WebKit.Components;
 public class WebKitGridViewItem
 {
     public required string? Title { get; set; }
-    public required string? Subtitle { get; set; }
     public required DirectusFile Image { get; set; } = new();
     public required string? Text { get; set; }
+    public string? Subtitle { get; set; }
     public string? Url { get; set; }
 }
