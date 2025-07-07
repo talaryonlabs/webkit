dd4c8d8095580b1ee80f4c118eaa6f8903e3f744
ferociousbyte <ferociousbyte@talaryonlabs.com>
2025-07-07 22:16:35 +0200

    Add WebKitExtensions for service and global configuration
    Introduce `WebKitExtensions` with methods to register WebKit services and configure global options. Enhances modularity and streamlines application setup.

---
+++ src/WebKit/Base/WebKitExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Talaryon.WebKit;
+
+public static class WebKitExtensions
+{
+    public static IServiceCollection AddWebKitComponents(this IServiceCollection services) =>
+        services
+            .AddOptions()
+            .AddSingleton<Services.WebKit.WebKit>()
+            .AddSingleton<Services.WebKit.IWebKit>(x => x.GetRequiredService<Services.WebKit.WebKit>());
+
+    public static void ConfigureGlobal<T>(this WebApplication app, Action<T> optionsConfigurator) where T : Services.WebKit.IWebKitOptions =>
+        app
+            .Services
+            .GetService<Services.WebKit.IWebKit>()?
+            .ConfigureGlobal<T>(optionsConfigurator);
+}
