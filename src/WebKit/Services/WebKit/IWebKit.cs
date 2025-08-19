e29dcded60c72404bc777397817bda4e2b740c25
ferociousbyte <ferociousbyte@talaryonlabs.com>
2025-08-19 16:22:28 +0200

    Refactor and migrate API offline and status code pages.
    Moved API offline and status code pages from the Website project to the WebKit library for better modularity and reuse. Updated all references and routes to adapt to the new structure. Introduced new layouts and configurations to integrate WebKit pages seamlessly.

--- src/WebKit/Services/WebKit/IWebKit.cs
+++ src/WebKit/Services/WebKit/IWebKit.cs
@@ -9,9 +9,7 @@ public interface IWebKit
     void ConfigureGlobal<T>(Action<T> optionsConfigurator) where T : IWebKitOptions;
     void ConfigureScoped<T>(Action<T> optionsConfigurator) where T : IWebKitOptions;
     
-    
     T GetOptions<T>() where T : IWebKitOptions;
-
 
     Type? GetComponent<TBase>() where TBase : IWebKitComponent;
 }
